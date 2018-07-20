3f905e0a90fb0fb8fd1ccb4a4400a60217360813
Mac <ranweiner1@gmail.com>
2018-07-20 12:20:51 +0300

    added basic functionality

--- FingerFood/Controllers/MainViewController.swift
+++ FingerFood/Controllers/MainViewController.swift
@@ -9,29 +9,74 @@
 import UIKit
 import Koloda
 
+private var numOfCards : Int = 5
 
-class MainViewController: UIViewController {
 
+class MainViewController: UIViewController , KolodaViewDataSource , KolodaViewDelegate {
+    
+
+
+    @IBOutlet weak var kolodaView: KolodaView!
+    
+    
+    private var dataSource : [UIImage] = {
+        var array: [UIImage] = []
+        for index in 0..<numOfCards {
+            array.append(UIImage(named: "Image\(index + 1)")!)
+        }
+        return array
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+        self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
     }
 
+    @IBAction func likeBtnPressed(_ sender: Any) {
+        kolodaView?.swipe(.right)
+    }
+    
+    @IBAction func disslikeBtnPressed(_ sender: Any) {
+        kolodaView?.swipe(.left)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return dataSource.count
     }
-    */
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        print("index = \(index)")
+        return UIImageView(image: dataSource[index])
+    }
+   
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        return .default
+        
+    }
+    
+    func koloda(_ koloda: KolodaView, didSwipeCardAt index: Int, in direction: SwipeResultDirection) {
+
+        switch direction {
+        case .left:
+            print("left")
+        case .right:
+            print("right")
+        default:
+            print("error")
+        }
+    }
+    
+    
+    
 
 }
+
